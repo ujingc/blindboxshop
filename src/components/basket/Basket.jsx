abf081cc57906ef7ccb485b67687ab9b4307ec83
yujing <yujing@yujings-MBP.lan>
2025-11-19 15:08:20 +0800

    BOX-003 get firebase object from firebase instance

--- src/components/basket/Basket.jsx
+++ src/components/basket/Basket.jsx
@@ -2,7 +2,8 @@
 import { BasketItem, BasketToggle } from '@/components/basket';
 import { Boundary, Modal } from '@/components/common';
 import { CHECKOUT_STEP_1 } from '@/constants/routes';
-import firebase from 'firebase/firebase';
+// import firebase from 'firebase/firebase';
+import firebase from '../../services/firebase'
 import { calculateTotal, displayMoney } from '@/helpers/utils';
 import { useDidMount, useModal } from '@/hooks';
 import React, { useEffect } from 'react';
